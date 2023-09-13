e7af7e31befaac38159b14edddc9c17f5c5a9d78
Harshpatidar245 <98421797+Harshpatidar245@users.noreply.github.com>
2023-09-14 00:56:57 +0530

    Add files via upload

---
+++ to-do-list.js
@@ -0,0 +1,16 @@
+let addtodobutton = document.getElementById('addtodo');
+let todocontainer = document.getElementById('todocontainer');
+let inputfield = document.getElementById('inputfield');
+
+addtodobutton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.innerText = inputfield.value;
+    todocontainer.appendChild(paragraph);
+    inputfield.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        todocontainer.removeChild(paragraph);
+    })
+})
